22e1e0d7fd0f6fefc45507186d74ec9b1636cf30
ADITYA SHINDE <adityashinde.0405@gmail.com>
2021-05-17 23:44:23 +0530

    Update DataShield.py

--- DataShield.py
+++ DataShield.py
@@ -4,8 +4,8 @@ from os import stat, remove
 print("****************************************************************************")
 print("\t \t \t MENU")
 print("***************************************************************************")
-print("\t \t 1. Data Encryption and Decryption")
-print("\t \t 2. Text File Encryption and Decryption ")
+print("\t \t 1. Data XOR Encryption and Decryption ")
+print("\t \t 2. Data  AES Encryption and Decryption ")
 print("***************************************************************************")
 a=int(input("Enter your choice"))
 if a==1:
@@ -28,25 +28,25 @@ if a==1:
         	print('Key for encryption : ', key)
         	
         	
-        	fin = open(path, 'rb') #Reading Binary value of fetched datafile
+        	fin = open(path, 'rb') #Reading Binary value of fetched file/image
         	
         	
-        	datafile = fin.read()
+        	image = fin.read()
         	fin.close()
         	
         	
         	
-        	datafile = bytearray(datafile) #typecasting
+        	image = bytearray(image)#typecasting
         
         	
-        	for index, values in enumerate(datafile):
-        		datafile[index] = values ^ key
+        	for index, values in enumerate(image):
+        		image[index] = values ^ key
         
         	
-        	fin = open(path, 'wb') #Writing Binary value of fetched datafile
+        	fin = open(path, 'wb') #Writing Binary value of fetched file/image
         	
         	
-        	fin.write(datafile)
+        	fin.write(image)
         	fin.close()
         	print('Encryption Done...')
         
@@ -68,24 +68,24 @@ if a==1:
         	print('Key for Decryption : ', key)
         	
         	
-        	fin = open(path, 'rb') #Reading Binary value of fetched file/datafile
+        	fin = open(path, 'rb') #Reading Binary value of fetched file/image
         	
         	
-        	datafile = fin.read() 
+        	image = fin.read()
         	fin.close()
         	
         	
-        	datafile = bytearray(datafile)
+        	image = bytearray(image)
         
         	
-        	for index, values in enumerate(datafile):     #Xor Applied
-        		datafile[index] = values ^ key
+        	for index, values in enumerate(image):
+        		image[index] = values ^ key
         
 
         	fin = open(path, 'wb')
         	
         	
-        	fin.write(datafile)
+        	fin.write(image)
         	fin.close()
         	print('Decryption Done...')
         
@@ -101,28 +101,27 @@ if a==2:
     print("2 for Decryption")
     print("##############################################################################################")
     h=int(input("Enter the Choice"))
-    fi=input("Enter the file location with extension")
+    jh=input("Enter the file location with extension")
     bufferSize = 64 * 1024 #by default 512 (max value 64*1024)
     password = input("Enter the password")
-    aext=f'{fi}.aes'
+    koi=f'{jh}.aes'
 
     if h==1:
         
         
     
-        with open(fi, "rb") as fIn:
-         with open(aext, "wb") as fOut:
+        with open(jh, "rb") as fIn:
+         with open(koi, "wb") as fOut:
              pyAesCrypt.encryptStream(fIn, fOut, password, bufferSize)
         print("Encryption done")
    
-    encFileSize = stat(aext).st_size
-    print(encFileSize)
+    
     if h==2:
     #DEcrypt................
-        encFileSize = stat(aext).st_size
+        encFileSize = stat(jh).st_size
         print(encFileSize)
         
-        with open(aext, "rb") as fIn:
+        with open(jh, "rb") as fIn:
             try:
                 aso=input("decrrypted file name")
                 with open(aso, "wb") as fOut:
